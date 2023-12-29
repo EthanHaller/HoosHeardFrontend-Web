e7cd72c956f092030e3f59458ba5ab45b1f5e194
EthanHaller <ttk4ey@virginia.edu>
2023-12-28 18:06:38 -0700

    refactor: change ResponseCards to ResponseCard so each card can be updated individually

---
+++ src/components/responses/ResponseCard.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircle, faHeart } from "@fortawesome/free-solid-svg-icons"
+import { faHeart as faUnliked, faComment as faUncommented } from "@fortawesome/free-regular-svg-icons"
+
+import "../../styles/responses.css"
+import { useAuth } from "../../AuthProvider"
+import axios from "axios"
+
+export default function ResponseCard({ response }) {
+    const { user } = useAuth()
+    const [isLiked, setisLiked] = useState(false)
+    const [numLikes, setNumLikes] = useState(false)
+
+    const handleLikeUnlike = (responseId) => {
+		if (isLiked) {
+			// unlike
+			axios.post(`${process.env.REACT_APP_BACKEND_URL}/likes/unlike`, {
+				email: user.user.email,
+				responseId: responseId
+			})
+			.then(res => {
+				setisLiked(false)
+                setNumLikes(numLikes-1)
+			})
+			.catch(err => {
+				console.error(err)
+			})
+		} else {
+			// like
+			axios.post(`${process.env.REACT_APP_BACKEND_URL}/likes/unlike`, {
+				email: user.user.email,
+				responseId: responseId
+			})
+			.then(res => {
+				setisLiked(true)
+                setNumLikes(numLikes+1)
+			})
+			.catch(err => {
+				console.error(err)
+			})
+		}
+	}
+
+    const timeAgo = calculateTimeAgo(response.createdAt)
+    const nLikes = nFormatter(numLikes)
+    const nComments = nFormatter(response.numComments, 1)
+
+	return (
+		<div key={response._id} className="card mb-3">
+			<div className="card-body">
+				<div className="d-inline-flex align-items-center mb-3">
+					<h5 className="m-0 text-primary">Anonymous</h5>
+					<FontAwesomeIcon icon={faCircle} className="circle-icon" />
+					<h6 className="m-0 text-secondary"> {timeAgo}</h6>
+				</div>
+				<p className="card-text">{response.text}</p>
+				<div className="container-fluid">
+					<div className="row">
+						<div className="col-10"></div>
+						<div className="col-1 p-0">
+							<div className="d-flex flex-column">
+								<button className="btn p-0" onClick={() => handleLikeUnlike(response._id)}>
+									<FontAwesomeIcon icon={isLiked ? faHeart : faUnliked} className="text-primary" />
+									<p className="m-0 text-primary text-center">{nLikes}</p>
+								</button>
+							</div>
+						</div>
+						<div className="col-1 p-0">
+							<div className="d-flex flex-column">
+								<button className="btn p-0">
+									<FontAwesomeIcon icon={faUncommented} className="text-primary" />
+									<p className="m-0 text-primary text-center">{nComments}</p>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+function calculateTimeAgo(timestamp) {
+	const now = new Date()
+	const posted = new Date(timestamp)
+
+	var ms_Min = 60 * 1000
+	var ms_Hour = ms_Min * 60
+
+	const diff = now - posted
+
+	if (diff < ms_Min) {
+		if (Math.round(diff / 1000) === 1) return Math.round(diff / 1000) + " second ago"
+		return Math.round(diff / 1000) + " seconds ago"
+	} else if (diff < ms_Hour) {
+		if (Math.round(diff / ms_Min) === 1) return Math.round(diff / ms_Min) + " minute ago"
+		return Math.round(diff / ms_Min) + " minutes ago"
+	} else {
+		if (Math.round(diff / ms_Hour) === 1) return Math.round(diff / ms_Hour) + " hour ago"
+		return Math.round(diff / ms_Hour) + " hours ago"
+	}
+}
+
+// https://stackoverflow.com/questions/9461621/format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900
+function nFormatter(num, digits) {
+	const lookup = [
+		{ value: 1, symbol: "" },
+		{ value: 1e3, symbol: "k" },
+		{ value: 1e6, symbol: "M" },
+		{ value: 1e9, symbol: "G" },
+		{ value: 1e12, symbol: "T" },
+		{ value: 1e15, symbol: "P" },
+		{ value: 1e18, symbol: "E" },
+	]
+	const rx = /\.0+$|(\.[0-9]*[1-9])0+$/
+	var item = lookup
+		.slice()
+		.reverse()
+		.find(function (item) {
+			return num >= item.value
+		})
+	return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0"
+}
--- src/pages/Responses.jsx
+++ src/pages/Responses.jsx
@@ -1,20 +1,51 @@
-import React from "react"
-import ResponseCards from "../components/responses/ResponseCards"
+import React, { useState } from "react"
 import PromptSidebar from "../components/PromptSidebar"
 import useFetch from "../hooks/useFetch"
 
 import "../styles/responses.css"
+import ResponseCard from "../components/responses/ResponseCard"
 
 export default function Responses() {
 	const { data, isLoading, error } = useFetch("/prompts/latest")
+	const { data: responseData, isLoading: isResponseLoading, error: responseError } = useFetch("/responses/")
+	const [sortOption, setSortOption] = useState("hot")
+
+	const handleSort = (option) => {
+		setSortOption(option)
+	}
+
+	if (isResponseLoading || !responseData) {
+		return <p>Loading responses...</p>
+	}
+
+	const responsesSorted = [...responseData.responses].sort((a, b) => {
+		if (sortOption === "recent") {
+			return new Date(b.createdAt) - new Date(a.createdAt)
+		} else {
+			return b.likes - a.likes
+		}
+	})
+
+	const responseCards = responsesSorted.map((response) => {
+		return <ResponseCard response={response} key={response._id}/>
+	})
 
 	return (
 		<>
 			<div className="container-fluid">
 				<div className="row view-height">
-					<PromptSidebar displayText={"ANONYMOUS RESPONSES TO..."} data={data} isLoading={isLoading} error={error}/>
+					<PromptSidebar displayText={"ANONYMOUS RESPONSES TO..."} data={data} isLoading={isLoading} error={error} />
 					<div className="col-lg-7 h-100 lightest">
-						<ResponseCards />
+						<span className="d-flex align-items-center mt-2 mb-3">
+							<button className={`btn ${sortOption === "hot" ? "active" : ""}`} onClick={() => handleSort("hot")}>
+								Hot
+							</button>
+							<p className="text-center p-0 m-0">|</p>
+							<button className={`btn ${sortOption === "recent" ? "active" : ""}`} onClick={() => handleSort("recent")}>
+								Recent
+							</button>
+						</span>
+						{responseCards}
 					</div>
 				</div>
 			</div>
