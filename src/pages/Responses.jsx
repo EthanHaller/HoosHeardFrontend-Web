d8e21b7467c0653b0d52d8f92ee053ef258de76f
EthanHaller <ttk4ey@virginia.edu>
2024-01-08 18:43:34 -0600

    feature: add navigation buttons to user answer and logout on resopnses page

--- src/pages/Responses.jsx
+++ src/pages/Responses.jsx
@@ -2,10 +2,16 @@ import React, { useState } from "react"
 import PromptSidebar from "../components/PromptSidebar"
 import useFetch from "../hooks/useFetch"
 import ResponseCard from "../components/responses/ResponseCard"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import "../styles/responses.css"
+import { Link } from "react-router-dom"
+import { faArrowRightFromBracket, faUserPen } from "@fortawesome/free-solid-svg-icons"
+import { useAuth } from "../AuthProvider"
 
 export default function Responses() {
+	const { user, logout } = useAuth()
+
 	const { data, isLoading, error } = useFetch("/prompts/latest")
 	const { data: responseData, isLoading: isResponseLoading, error: responseError } = useFetch("/responses/")
 	const [sortOption, setSortOption] = useState("hot")
@@ -27,7 +33,7 @@ export default function Responses() {
 	})
 
 	const responseCards = responsesSorted.map((response) => {
-		return <ResponseCard response={response} key={response._id}/>
+		return <ResponseCard response={response} key={response._id} />
 	})
 
 	return (
@@ -36,13 +42,20 @@ export default function Responses() {
 				<div className="row view-height">
 					<PromptSidebar displayText={"ANONYMOUS RESPONSES TO..."} data={data} isLoading={isLoading} error={error} />
 					<div className="col-lg-7 lightest">
-						<span className="d-flex align-items-center mt-2 mb-3">
-							<button className={`btn ${sortOption === "hot" ? "active" : ""}`} onClick={() => handleSort("hot")}>
+						<span className="d-flex align-items-center mx-2 my-3">
+							<button className={`sort-btn ${sortOption === "hot" ? "active" : ""} mx-2`} onClick={() => handleSort("hot")}>
 								Hot
 							</button>
 							<p className="text-center p-0 m-0">|</p>
-							<button className={`btn ${sortOption === "recent" ? "active" : ""}`} onClick={() => handleSort("recent")}>
+							<button className={`sort-btn ${sortOption === "recent" ? "active" : ""} mx-2`} onClick={() => handleSort("recent")}>
 								Recent
+							</button>
+							<span className="flex-grow-1"></span>
+							<Link to={"/my-answer"} aria-labelledby="My Answer" className="mx-2">
+								<FontAwesomeIcon icon={faUserPen} className="fontawesome-btn"/>
+							</Link>
+							<button className="logout-btn mx-2" onClick={() => logout()}>
+								<FontAwesomeIcon icon={faArrowRightFromBracket} className="fontawesome-btn"/>
 							</button>
 						</span>
 						{responseCards}
