8e08e5bbe957d4d19c22bfb93784df0c2064805d
EthanHaller <ttk4ey@virginia.edu>
2024-01-14 15:07:12 -0500

    style: center user answer form

--- src/pages/UserAnswer.jsx
+++ src/pages/UserAnswer.jsx
@@ -66,48 +66,50 @@ export default function UserAnswer() {
 								<FontAwesomeIcon icon={faArrowRightFromBracket} className="fontawesome-btn" />
 							</button>
 						</span>
-						<form className="d-flex flex-column mx-3">
-							<div className="form-group">
-								<label htmlFor="userResponseTextarea" className="text-primary textarea-label">
-									Your Answer
-								</label>
-								<textarea className="custom-textarea" id="userResponseTextarea" rows="12" onChange={handleTextareaChange}></textarea>
-							</div>
-							<button type="button" className="custom-btn align-self-end mt-1" onClick={() => setShowModal(true)}>
-								Submit
-							</button>
-							<div
-								className={`modal fade ${showModal ? "show" : ""}`}
-								style={{ display: showModal ? "block" : "none" }}
-								tabIndex="-1"
-								role="dialog"
-							>
-								<div className="modal-dialog modal-dialog-centered" role="document">
-									<div className="modal-content">
-										<div className="modal-header dark">
-											<h5 className="modal-title text-primary">Confirm Submission</h5>
-											<button
-												type="button"
-												className="btn-close"
-												data-dismiss="modal"
-												aria-label="Close"
-												onClick={() => setShowModal(false)}
-											></button>
-										</div>
-										<div className="modal-body text-primary medium">Are you sure you are ready to submit your answer?</div>
-										<div className="modal-footer medium">
-											<button type="button" className="custom-btn" data-dismiss="modal" onClick={() => setShowModal(false)}>
-												Cancel
-											</button>
-											<button type="submit" className="custom-btn" onClick={handleConfirmSubmit}>
-												Submit
-											</button>
+						<div className="d-flex flex-column justify-content-center h-100">
+							<form className="d-flex flex-column mx-3">
+								<div className="form-group">
+									<label htmlFor="userResponseTextarea" className="text-primary textarea-label">
+										Your Answer
+									</label>
+									<textarea className="custom-textarea" id="userResponseTextarea" rows="12" onChange={handleTextareaChange}></textarea>
+								</div>
+								<button type="button" className="custom-btn align-self-end mt-1" onClick={() => setShowModal(true)}>
+									Submit
+								</button>
+								<div
+									className={`modal fade ${showModal ? "show" : ""}`}
+									style={{ display: showModal ? "block" : "none" }}
+									tabIndex="-1"
+									role="dialog"
+								>
+									<div className="modal-dialog modal-dialog-centered" role="document">
+										<div className="modal-content">
+											<div className="modal-header dark">
+												<h5 className="modal-title text-primary">Confirm Submission</h5>
+												<button
+													type="button"
+													className="btn-close"
+													data-dismiss="modal"
+													aria-label="Close"
+													onClick={() => setShowModal(false)}
+												></button>
+											</div>
+											<div className="modal-body text-primary medium">Are you sure you are ready to submit your answer?</div>
+											<div className="modal-footer medium">
+												<button type="button" className="custom-btn" data-dismiss="modal" onClick={() => setShowModal(false)}>
+													Cancel
+												</button>
+												<button type="submit" className="custom-btn" onClick={handleConfirmSubmit}>
+													Submit
+												</button>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-							<div className={`modal-backdrop fade ${showModal ? "show" : ""}`} style={{ display: showModal ? "block" : "none" }}></div>
-						</form>
+								<div className={`modal-backdrop fade ${showModal ? "show" : ""}`} style={{ display: showModal ? "block" : "none" }}></div>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
