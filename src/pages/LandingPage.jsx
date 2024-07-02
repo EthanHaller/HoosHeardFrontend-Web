b3e7a7221aa850b18da9f3d15126b0dbc1b459f1
EthanHaller <ttk4ey@virginia.edu>
2024-07-02 19:37:05 -0400

    fix: add disclaimer to landing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -36,8 +36,8 @@ export default function LandingPage() {
 						</div>
 						<div className="middle-row">
 							<p className="landing-description mx-4 mb-0">
-								Welcome to HoosHeard, a unique social media platform designed for venting! Every day, you are presented with a
-								thought-provoking question to answer. The catch? You must submit your response before gaining access to all other anonymous
+								Welcome to HoosHeard, an anonymous social media platform designed to help your voice be heard! Every day, you are presented with a
+								thought-provoking question to answer. The catch? You must submit your response before you can see all other anonymous
 								responses. Join HoosHeard to share, connect, and be heard!
 							</p>
 						</div>
@@ -55,6 +55,7 @@ export default function LandingPage() {
 									onFailure={(error) => console.error("Google login error:", error)}
 								/>
 							</div>
+							<p className="disclaimer mt-3">We will not share your information with anyone.</p>
 						</div>
 					</div>
 					<div className="col-xl-4 d-none d-xl-block lightest shapedividers_com-1228"></div>
